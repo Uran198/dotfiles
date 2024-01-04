02c8e2c352e136449f995bf2e21da9d8f4f70b71
Attila <uran198@gmail.com>
2024-01-04 22:39:08 +0100

    Add pylsp.

--- vim/.config/nvim/lua/custom/lsp.lua
+++ vim/.config/nvim/lua/custom/lsp.lua
@@ -57,6 +57,8 @@ function M.setup()
     };
   };
 
+  lspconfig.pylsp.setup { };
+
   -- Use LspAttach autocommand to only map the following keys
   -- after the language server attaches to the current buffer
   vim.api.nvim_create_autocmd('LspAttach', {
